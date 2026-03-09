29b6353ea54558808cac7f38e8eee4961d63c1a2
Vidhatra Shukla <shuklavidhatra@gmail.com>
2026-03-09 20:32:17 +0530

    commit:new portfolio

---
+++ certificates.js
@@ -0,0 +1,22 @@
+const certificates = [
+    {
+        name: "AI Python for Beginners",
+        issuer: "DeepLearning.AI",
+        file: "certificates/certificate.png"
+    },
+    {
+        name: "Build with Andrew",
+        issuer: "DeepLearning.AI",
+        file: "certificates/certificate 1.png"
+    },
+    {
+        name: "Generative AI for Everyone",
+        issuer: "DeepLearning.AI",
+        file: "certificates/certificate 2.png"
+    },
+    {
+        name: "Orchestrating Workflows for GenAI Applications",
+        issuer: "DeepLearning.AI",
+        file: "certificates/certificate.png"
+    }
+];
